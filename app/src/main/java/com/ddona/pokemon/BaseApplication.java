e17057adfc45fe77dc1797bebb77311900be5344
Trung Doan <doanptand@gmail.com>
2020-09-13 18:14:19 +0700

    add dependencies

---
+++ app/src/main/java/com/ddona/pokemon/BaseApplication.java
@@ -0,0 +1,11 @@
+package com.ddona.pokemon;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
