7363d77b9aa01b6e2c8b16f26bc1f939f4916674
Trung Doan <doanptand@gmail.com>
2020-09-13 19:59:31 +0700

    add repository

---
+++ app/src/main/java/com/ddona/pokemon/repository/PokemonRepository.java
@@ -0,0 +1,39 @@
+package com.ddona.pokemon.repository;
+
+
+import androidx.lifecycle.LiveData;
+
+import com.ddona.pokemon.db.PokemonDao;
+import com.ddona.pokemon.model.Pokemon;
+import com.ddona.pokemon.model.PokemonResponse;
+import com.ddona.pokemon.network.PokemonService;
+
+import java.util.List;
+
+import retrofit2.Call;
+
+public class PokemonRepository {
+    private PokemonDao pokemonDao;
+    private PokemonService pokemonService;
+
+    public PokemonRepository(PokemonDao pokemonDao, PokemonService pokemonService) {
+        this.pokemonDao = pokemonDao;
+        this.pokemonService = pokemonService;
+    }
+
+    public Call<PokemonResponse> getPokemons() {
+        return pokemonService.getPokemons();
+    }
+    public void insertPokemon(Pokemon pokemon) {
+        pokemonDao.insertPokemon(pokemon);
+    }
+    public void deleteOnePokemon(String name) {
+        pokemonDao.deleteOnePokemon(name);
+    }
+    public void deleteAllPokemon() {
+        pokemonDao.deleteAllPokemons();
+    }
+    public LiveData<List<Pokemon>> getFavoritePokemons() {
+        return pokemonDao.getAllPokemons();
+    }
+}
