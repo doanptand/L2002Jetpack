d7918ab93ecf6c33fec34339e08ea1046c311f90
Trung Doan <doanptand@gmail.com>
2020-09-13 18:46:49 +0700

    implement net work module with retrofit

---
+++ app/src/main/java/com/ddona/pokemon/network/PokemonClient.java
@@ -0,0 +1,23 @@
+package com.ddona.pokemon.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class PokemonClient {
+
+    private static Retrofit INSTANCE;
+
+    private PokemonClient() {
+
+    }
+
+    public static Retrofit getInstance(String baseUrl) {
+        if (INSTANCE == null) {
+            INSTANCE = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return INSTANCE;
+    }
+}
--- app/src/main/java/com/ddona/pokemon/ui/MainActivity.java
+++ app/src/main/java/com/ddona/pokemon/ui/MainActivity.java
@@ -3,8 +3,15 @@ package com.ddona.pokemon.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.ddona.pokemon.R;
+import com.ddona.pokemon.model.PokemonResponse;
+import com.ddona.pokemon.network.PokemonModule;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //FIXME only for test remove in product
+        //TODO get list and display it
+//
+//        PokemonModule.getInstance().getPokemons().enqueue(new Callback<PokemonResponse>() {
+//            @Override
+//            public void onResponse(Call<PokemonResponse> call, Response<PokemonResponse> response) {
+//                if (response.isSuccessful()) {
+//                    Log.d("doanpt", "result:" + response.body().getCount());
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<PokemonResponse> call, Throwable t) {
+//
+//            }
+//        });
     }
 }
