b9b3fdd459dd51d6533a2de8244262267409baa5
Trung Doan <doanptand@gmail.com>
2020-09-30 20:16:00 +0700

    on click for button using databinding

---
+++ app/src/main/java/com/ddona/pokemon/callback/OnProfileChangeListener.java
@@ -0,0 +1,6 @@
+package com.ddona.pokemon.callback;
+
+public interface OnProfileChangeListener {
+
+    void onProfileChange();
+}
--- app/src/main/java/com/ddona/pokemon/ui/ProfileActivity.java
+++ app/src/main/java/com/ddona/pokemon/ui/ProfileActivity.java
@@ -5,12 +5,16 @@ import androidx.databinding.DataBindingUtil;
 
 import android.database.DatabaseUtils;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.ddona.pokemon.R;
+import com.ddona.pokemon.callback.OnProfileChangeListener;
 import com.ddona.pokemon.databinding.ActivityProfileBinding;
 import com.ddona.pokemon.model.User;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity
+        implements OnProfileChangeListener {
     private ActivityProfileBinding binding;
 
     @Override
@@ -21,6 +25,11 @@ public class ProfileActivity extends AppCompatActivity {
                 "doanpt93cntt@gmail.com",
                 "https://image.thanhnien.vn/768/uploaded/ngocthanh/2020_07_13/ngoctrinhmuonsinhcon1_swej.jpg");
         binding.setUser(user);
+        binding.setCallback(this);
+    }
 
+    @Override
+    public void onProfileChange() {
+        Toast.makeText(this, "On data changed", Toast.LENGTH_LONG).show();
     }
 }
